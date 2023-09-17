0d5ea8c81095f1fa8c74b741ea60aef30ab8f964
dannarew <danielnarew05@outlook.com>
2023-09-17 13:05:02 -0400

    lol

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -2,11 +2,13 @@
 #create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
+def main(): ''' 
+    print("My favorite color is: ", favColor)
+    favColor = ("burgundy")
+    # won't work because lines are executed top to bottom, so the variable isn't defined if placed beforehand ''' 
 
-
-def main():
-
-
+birthday = "01292005"
+print("My birthday is: ", birthday)
 
     
     

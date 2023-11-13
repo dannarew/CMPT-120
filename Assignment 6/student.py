b927623911f4f676678a6e5b34274f9a572e0d9c
dannarew <danielnarew05@outlook.com>
2023-11-13 00:00:36 -0500

    oof

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -24,15 +24,48 @@ create a function because this college is a wacky one- every day they generate a
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
 
-class student:
-    
-    
-    
-    
-    
-    
+import random
+
+class Student:
+    def __init__(self, name, year, major, gpa):
+        self.name = name
+        self.student_id = random.randrange(1, 1000, 1)
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def honors(self):
+        if self.gpa > 3.5:
+            print("is eligible for the honors program!")
+
+        else:
+            print("does not qualify for the honors program.")
+
+
+    def freeLunch(self, generated_id):
+        if generated_id == self.student_id:
+            print("Yay free lunch!")
+        else:
+            print("Loser!")
+
 def main():
-    #create three students and check if they get free lunch and if they qualify for honors
-    
-    
-main()
+    # create three students
+    student1 = Student("Dan", "Freshman", "Computer Science", 3.8)
+    student2 = Student("Bob", "Sophomore", "Engineering", 3.2)
+    student3 = Student("Joe", "Junior", "Biology", 4.0)
+
+
+    generated_id = random.randrange(1, 1000, 1)
+
+    student1.freeLunch(generated_id)
+    student1.honors()
+
+    student2.freeLunch(generated_id)
+    student2.honors()
+
+    student3.freeLunch(generated_id)
+    student3.honors()
+
+
+    main()
+
